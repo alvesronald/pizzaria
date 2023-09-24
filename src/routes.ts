66ebdfc8ae635405657dbed07a697cf4d6bbe81f
Ronald Alves <alvesronald@MacBook-de-Ronald.local>
2023-09-24 16:10:11 -0300

    feat: authenticate user

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
+import { AuthUserController } from "./controllers/user/AuthUserController";
 import { CreateUserController } from "./controllers/user/CreateUserController";
 
 const router = Router();
 
 //User Routers
 router.post("/users", new CreateUserController().handle);
+router.post("/auth", new AuthUserController().handle);
 
 export { router };
